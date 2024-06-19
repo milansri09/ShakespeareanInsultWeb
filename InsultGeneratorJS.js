7429770839c118f4a8520315b15f92219952d46c
milansri09 <53534531+milansri09@users.noreply.github.com>
2024-06-18 20:19:17 -0400

    Update InsultGeneratorJS.js

--- InsultGeneratorJS.js
+++ InsultGeneratorJS.js
@@ -1,24 +1,37 @@
 // JavaScript source code
 
 function insultSpreadsheet() {
-    fetch("insults.csv");
+    var request = fetch("https://milansri09.github.io/ShakespeareanInsultWeb/insults.csv");
+    request.then((response) => {
+        return response.text();
+    }).then((data) => {
+        console.log(data);
+        var rows = data.split("\n");
+        for (var i = 0; i < rows.length; i++) {
+            var row = rows[i];
+            var cols = row.split(",");
+            insultColumnA.push(cols[0]);
+            insultColumnB.push(cols[1]);
+            insultColumnC.push(cols[2]);
+        }
+    })
 }
 insultSpreadsheet();
 
 var insultColumnA = [];
-insultColumnA.push("Here's");
+/* insultColumnA.push("Here's");
 insultColumnA.push("An");
-insultColumnA.push("Insult");
+insultColumnA.push("Insult"); */
 
 var insultColumnB = [];
-insultColumnB.push("Yet");
+/* insultColumnB.push("Yet");
 insultColumnB.push("Another");
-insultColumnB.push("Insult");
+insultColumnB.push("Insult"); */
 
 var insultColumnC = [];
-insultColumnC.push("One");
+/*insultColumnC.push("One");
 insultColumnC.push("Final");
-insultColumnC.push("Offense");
+insultColumnC.push("Offense"); */
 
 var isInsultVisible = false;
 
@@ -39,7 +52,7 @@ function generateInsult() {
     var insultA = generalRandomElement(insultColumnA); 
     var insultB = generalRandomElement(insultColumnB);
     var insultC = generalRandomElement(insultColumnC);
-    var finalInsult = "You " + insultA + " " + insultB + " " + insultC;
+    var finalInsult = "Thou " + insultA + " " + insultB + " " + insultC + "!";
     document.getElementById("insultText").innerHTML = finalInsult;
     document.getElementById("insultText").classList.add("show");
     isInsultVisible = true;
